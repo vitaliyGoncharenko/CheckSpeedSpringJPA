7c128470992da13304812def422b2a008447f28c
Vitaliy <vitalik300588@rambler,ru>
2016-11-27 21:50:29 +0200

    add logger to aspect

--- service/src/main/java/goncharenko/gvv/aspect/SpeedAspect.java
+++ service/src/main/java/goncharenko/gvv/aspect/SpeedAspect.java
@@ -3,6 +3,8 @@ package goncharenko.gvv.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class SpeedAspect {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpeedAspect.class);
 
     @Around("execution(* goncharenko.gvv.service.ModelRepositoryServiceImpl.* (..))")
     public void aroundRepositoryMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
@@ -19,10 +22,9 @@ public class SpeedAspect {
         proceedingJoinPoint.proceed();
         long timeAfter = System.currentTimeMillis();
         int difference = (int) (timeAfter - timeBefore);
-        System.out.println("Process " + proceedingJoinPoint.getSignature().toShortString() + ", is execute " + difference + " millisecond");
-//        LOGGER.info("***********************************************************************************************************");
-//        LOGGER.info("Process " + proceedingJoinPoint.getSignature().toShortString() + ", is execute " + difference + " millisecond");
-//        LOGGER.info("***********************************************************************************************************");
+        LOGGER.info("***********************************************************************************************************");
+        LOGGER.info("Process " + proceedingJoinPoint.getSignature().toShortString() + ", is executed " + difference + " millisecond");
+        LOGGER.info("***********************************************************************************************************");
     }
 
 }
